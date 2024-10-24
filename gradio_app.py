ac06ee3fc29f0ac9226a73fc0ac2ea81414f26ea
zx_li <zx_li@nreal.ai>
2024-10-24 05:31:30 +0000

    feat(mdoel): add lcm model

--- gradio_app.py
+++ gradio_app.py
@@ -14,6 +14,10 @@ from tsr.system import TSR
 from tsr.utils import remove_background, resize_foreground, to_gradio_3d_orientation
 
 import argparse
+import torch
+import cv2
+import numpy as np
+from diffusers import LCMScheduler, AutoPipelineForText2Image
 
 
 if torch.cuda.is_available():
@@ -27,12 +31,27 @@ model = TSR.from_pretrained(
     weight_name="model.ckpt",
 )
 
+
+
+
 # adjust the chunk size to balance between speed and memory usage
 model.renderer.set_chunk_size(8192)
 model.to(device)
 
 rembg_session = rembg.new_session()
 
+# t2i model 
+model_id = "AI-ModelScope/stable-diffusion-xl-base-1.0"
+adapter_id = "latent-consistency/lcm-lora-sdxl"
+from modelscope.hub.snapshot_download import snapshot_download
+model_dir = snapshot_download(model_id)
+sd_pipe = AutoPipelineForText2Image.from_pretrained(model_dir, torch_dtype=torch.float16, variant="fp16")
+sd_pipe.enable_model_cpu_offload()
+sd_pipe.scheduler = LCMScheduler.from_config(sd_pipe.scheduler.config)
+sd_pipe.to("cuda")
+# load and fuse lcm lora
+sd_pipe.load_lora_weights(adapter_id)
+sd_pipe.fuse_lora()
 
 def check_input_image(input_image):
     if input_image is None:
@@ -57,7 +76,13 @@ def preprocess(input_image, do_remove_background, foreground_ratio):
             image = fill_background(image)
     return image
 
-
+def generate_image(prompt, negtivate_prompt, guidance_scale=1, num_inference_steps=10):
+    image = sd_pipe(prompt=prompt, negtivate_prompt = negtivate_prompt,  num_inference_steps=num_inference_steps, guidance_scale=guidance_scale, height=1024, width=1024, generator=torch.Generator("cpu").manual_seed(0)).images[0]
+    # 将 PIL 图像转换为 NumPy 数组
+    numpy_array = np.array(image)
+    # 使用 OpenCV 从 NumPy 数组创建 cv::Mat 对象
+    image = cv2.cvtColor(numpy_array, cv2.COLOR_RGB2RGBA)
+    return image
 def generate(image, mc_resolution, formats=["obj", "glb"]):
     scene_codes = model(image, device=device)
     mesh = model.extract_mesh(scene_codes, True, resolution=mc_resolution)[0]
@@ -88,6 +113,13 @@ with gr.Blocks(title="TripoSR") as interface:
     3. Otherwise, please disable "Remove Background" option only if your input image is RGBA with transparent background, image contents are centered and occupy more than 70% of image width or height.
     """
     )
+    with gr.Row():
+        common_negtivate_prompt = "worst quality, normal quality, low quality, low res, blurry, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch ,duplicate, ugly, monochrome, horror, geometry, mutation, disgusting"
+        prompt_input = gr.Textbox(label="image prompt", value="a cartoonish teddy bear picture with a white background, enhanced with 3D effects to give it a playful, three-dimensional appearance. The focus should be on capturing the essence of a cuddly teddy bear in a vivid and imaginative way, suitable for a children's book illustration or a fun, whimsical wallpaper")
+        neg_prompt_input = gr.Textbox(label="negtivate prompt", value=common_negtivate_prompt)
+        guidance_scale = gr.Number(label="guidance scale", value=1.0, minimum=0, maximum=10)
+        num_steps = gr.Number(label="inference steps", value=10, minimum=4, maximum=20)
+        sd_button = gr.Button("生成图像")
     with gr.Row(variant="panel"):
         with gr.Column():
             with gr.Row():
@@ -157,6 +189,7 @@ with gr.Blocks(title="TripoSR") as interface:
             label="Examples",
             examples_per_page=20,
         )
+    sd_button.click(generate_image, [prompt_input, neg_prompt_input,  guidance_scale, num_steps], input_image)
     submit.click(fn=check_input_image, inputs=[input_image]).success(
         fn=preprocess,
         inputs=[input_image, do_remove_background, foreground_ratio],
